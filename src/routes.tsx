cce7908c63a8f5c9aa615e8ef08f4f5862af185e
Lucas Medici <lucasrpmedici@gmail.com>
2023-04-25 21:47:24 -0300

    Iniciando controle de permissoes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
-import React, { useContext } from "react";
+import React, { useContext, useRef } from "react";
 import { AuthProvider, AuthContext } from './context/auth'
-import { Route, Routes, Router, Navigate } from "react-router-dom";
+import { Route, Routes, Router, Navigate, useNavigate } from "react-router-dom";
+import {Toast} from 'primereact/toast'
 
 // Pages
 import HomeComercial from "./pages/HomeComercial/index";
@@ -12,23 +13,60 @@ import HomeRelatorios from "./pages/HomeRelatorios";
 import Login from "./pages/Login";
 
 const Rotas: React.FC = () => {
+  const navigate = useNavigate();
+
+  const ROLES = {
+    COMERCIAL: 'Comercial User',
+    FINANCEIRO: 'Financeiro User',
+    ADMIN: 'ADMIN User'
+  }
+  const CURRENT_USER_ROLE =  ROLES.COMERCIAL;
+  
+  
+  
+
   const Private = ({ children }) => {
     const { authenticated } = useContext(AuthContext);
 
-    return authenticated ? children : <Navigate to='/' />
+    // return authenticated ? children : <Navigate to='/' />
+    return children
   }
+
+
+
+
+  const RoleComercial = ({ children }) => {
+    
+    return  CURRENT_USER_ROLE == ROLES.COMERCIAL ? children : <Navigate to='/home' />
+
+  }
+
+  const RoleFinanceiro = ({ children }) => {
+    
+    return  CURRENT_USER_ROLE == ROLES.FINANCEIRO ? children :  <Navigate to='/home' />
+
+  }
+
+
+  // const RoleADMIN = ({ children }) => {
+    
+  //   return  CURRENT_USER_ROLE == ROLES.ADMIN ? children : <Navigate to='/' />
+
+  // }
+
+
 
   return (
     <div>
         <AuthProvider>
             <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/home" element={<Private><HomeComercial /></Private>} />
-                <Route path="/cadastro/cliente" element={<Private><CadastroCliente /></Private>} />
-                <Route path="/cadastro/venda" element={<Private><CadastroVenda /></Private>} />
-                <Route path="/listagem/venda" element={<Private><ListagemVendas /></Private>} />
-                <Route path="/listagem/cliente" element={<Private><ListaClienteUsuario/></Private>} />
-                <Route path="/relatorios" element={<Private><HomeRelatorios/></Private>} />
+                <Route path="/home" element={<Private><RoleComercial children={<HomeComercial />}/></Private>} />
+                <Route path="/cadastro/cliente" element={<Private><RoleComercial children={<CadastroCliente />}/></Private>} /> 
+                <Route path="/cadastro/venda" element={<Private><RoleComercial children={<CadastroVenda />}/></Private>} />
+                <Route path="/listagem/venda" element={<Private><RoleFinanceiro children={<ListagemVendas />}/></Private>} />
+                <Route path="/listagem/cliente" element={<Private><RoleComercial children={<ListaClienteUsuario />}/></Private>} />
+                <Route path="/relatorios" element={<Private><RoleFinanceiro children={<HomeRelatorios />}/></Private>} />
             </Routes>
         </AuthProvider>
     </div>
