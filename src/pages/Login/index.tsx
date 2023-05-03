20bf72df9a05d8d43d45ff6deff6baa738458a3d
ThiagoOAL <thiago-leite01@hotmail.com>
2023-05-03 10:16:14 -0300

    feat - ajuste para autenticar no serviço de cliente

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -33,7 +33,7 @@ const Login: React.FC = () => {
     const { authenticated, login } = useContext<AuthContextData>(AuthContext);
 
     const onSubmit = async (data : any) => {
-        const Cpf = String(data.Cpf)
+        const Cpf = String(data.Cpf).replace(/\D/g, "")
         const Password = String(password);
         await login(Cpf, Password);
         
