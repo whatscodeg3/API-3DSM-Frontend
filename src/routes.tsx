15d1294768ed9ec54346320df34f334923ed347e
Jonatas Mathias <jonatasmathiasdallo@gmail.com>
2023-05-14 14:13:30 -0300

    Logout caso token expire

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,6 +1,7 @@
-import React, { useContext, useRef, useState } from "react";
+import React, { useContext, useRef, useState, useEffect } from "react";
 import { AuthProvider, AuthContext } from './context/auth'
 import { Route, Routes, Router, Navigate } from "react-router-dom";
+import axios from "axios";
 
 // Pages
 import HomeComercial from "./pages/HomeComercial/index";
@@ -14,28 +15,53 @@ import { Toast } from "primereact/toast";
 import CadastroFuncionario from "./pages/CadastroFuncionario";
 import { PermissionGateRoutes } from "./context/permission-gate";
 
-
 const Rotas: React.FC = () => {
   const toast = useRef(null)
   const setToastContent = (content) =>{
     toast.current.show(content)
   }
 
-
+  const [redirect, setRedirect] = useState(true);
   const Private = ({ children, permissions }) => {
     const { authenticated } = useContext(AuthContext);
     const hasPermissions = PermissionGateRoutes(permissions)
   
-    if (authenticated && hasPermissions) {
+    if (authenticated && hasPermissions && redirect == true) {
       return children
     } else if (authenticated && hasPermissions==false) {
       return <Navigate to='/home' />
     } else {
+      setRedirect(true)
       return <Navigate to='/' />
     }
   }
 
+  const tokenClient = localStorage.getItem("tokenClient");
+  const tokenPurchases = localStorage.getItem("tokenPurchases");
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("teste")
+      axios.get("http://localhost:8080/check", {
+          headers: {
+            Authorization: `Bearer ${tokenClient}`,
+          },
+        }).catch((error) => {
+            setRedirect(false); 
+        });
 
+      axios.get("http://localhost:8081/check", {
+          headers: {
+            Authorization: `Bearer ${tokenPurchases}`,
+          },
+        }).catch((error) => {
+            setRedirect(false); 
+        });
+    }, 60000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [redirect]); 
 
   return (
     <div>
